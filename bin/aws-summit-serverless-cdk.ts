152f2afe9f65c15c23236e95183421e438222948
tomerjann <tomer.jann@gmail.com>
2020-06-20 13:18:10 +0300

    initial commit

--- bin/aws-summit-serverless-cdk.ts
+++ bin/aws-summit-serverless-cdk.ts
@@ -1,7 +1,19 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { AwsSummitServerlessCdkStack } from '../lib/aws-summit-serverless-cdk-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { AwsSummitServerlessCdkStack } from "../lib/aws-summit-serverless-cdk-stack";
 
 const app = new cdk.App();
-new AwsSummitServerlessCdkStack(app, 'AwsSummitServerlessCdkStack');
+
+new AwsSummitServerlessCdkStack(app, "prod", {
+  prod: true,
+  env: {
+    region: "eu-west-1",
+  },
+});
+
+new AwsSummitServerlessCdkStack(app, "stg", {
+  prod: false,
+  env: {
+    region: "eu-west-1",
+  },
+});
--- lib/aws-summit-serverless-cdk-stack.ts
+++ lib/aws-summit-serverless-cdk-stack.ts
@@ -1,9 +1,95 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigw from "@aws-cdk/aws-apigateway";
+import * as dynamodb from "@aws-cdk/aws-dynamodb";
+
+interface EnvStackProps extends cdk.StackProps {
+  prod: boolean;
+}
+
+const envVars = (prod: boolean) => {
+  const prodEnvs = {
+    dynamoDbReadWrite: 200,
+    apiGatewayName: "PROD_cdk_api",
+    tableName: "PROD_cdk_users",
+    lambdaVars: { TABLE_NAME: "PROD_cdk_users" },
+    concurrency: 100,
+  };
+
+  const stgEnvs = {
+    dynamoDbReadWrite: 200,
+    apiGatewayName: "STG_cdk_api",
+    tableName: "STG_cdk_users",
+    lambdaVars: { TABLE_NAME: "STG_cdk_users" },
+    concurrency: 100,
+  };
+
+  return prod ? prodEnvs : stgEnvs;
+};
 
 export class AwsSummitServerlessCdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: EnvStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const environmentVars = envVars(props?.prod ?? false);
+
+    // --- the dynamo db ---
+    const table = new dynamodb.Table(this, "people", {
+      partitionKey: { name: "name", type: dynamodb.AttributeType.STRING },
+      tableName: environmentVars.tableName,
+      readCapacity: environmentVars.dynamoDbReadWrite,
+      billingMode: dynamodb.BillingMode.PROVISIONED,
+    });
+
+    // --- api gateway ---
+    const api = new apigw.RestApi(this, environmentVars.apiGatewayName);
+
+    // --- greeter lambda ---
+    const welcomeLambda = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset("lambda"),
+      environment: environmentVars.lambdaVars,
+      reservedConcurrentExecutions: environmentVars.concurrency,
+      handler: "hello.handler",
+    });
+
+    // greeter lambda integration
+    const apiHelloInteg = new apigw.LambdaIntegration(welcomeLambda);
+    const apiHello = api.root.addResource("hello");
+    apiHello.addMethod("GET", apiHelloInteg);
+
+    // --- user input lambda ---
+    const createLambda = new lambda.Function(this, "CreateHandler", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset("lambda"),
+      environment: environmentVars.lambdaVars,
+      reservedConcurrentExecutions: environmentVars.concurrency,
+      handler: "createUser.handler",
+    });
+
+    // user input lambda integration
+    const apiCreateInteg = new apigw.LambdaIntegration(createLambda);
+    const apiCreate = api.root.addResource("create");
+    apiCreate.addMethod("POST", apiCreateInteg);
+
+    // --- table permissions ---
+    table.grantReadWriteData(createLambda);
+
+    // --- user read lambda ---
+    const readLambda = new lambda.Function(this, "ReadHandler", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset("lambda"),
+      environment: environmentVars.lambdaVars,
+      reservedConcurrentExecutions: environmentVars.concurrency,
+      handler: "readUser.handler",
+    });
+
+    // user read lambda integration
+    const apiReadInteg = new apigw.LambdaIntegration(readLambda);
+    const apiRead = api.root.addResource("read");
+    apiRead.addMethod("GET", apiReadInteg);
+
+    // --- table permissions ---
+    table.grantReadData(readLambda);
   }
 }
